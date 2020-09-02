c7c575bc10cc71edfe93df0ea5c1cbba1b19a309
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-09-02 18:42:51 +0300

    Fix deprecation warnings from new Java version

--- multiapps-common/src/test/java/org/cloudfoundry/multiapps/common/util/MapUtilTest.java
+++ multiapps-common/src/test/java/org/cloudfoundry/multiapps/common/util/MapUtilTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.cloudfoundry.multiapps.common.ContentException;
@@ -12,28 +11,23 @@ import org.junit.jupiter.api.Test;
 
 class MapUtilTest {
 
-    public static final Map<String, Object> testParameters = new HashMap<String, Object>() {
-        {
-            put("trueFlag", new Boolean(true));
-            put("falseFlag", new Boolean(false));
-            put("emptyFlag", null);
-            put("incorrectTypeFlag1", "false");
-            put("incorrectTypeFlag2", "1");
-        }
-    };
+    public static final Map<String, Object> TEST_PARAMETERS = Map.ofEntries(Map.entry("trueFlag", true), Map.entry("falseFlag", false),
+                                                                            Map.entry("emptyFlag", null),
+                                                                            Map.entry("incorrectTypeFlag1", "false"),
+                                                                            Map.entry("incorrectTypeFlag2", "1"));
 
     @Test
     void testParseBooleanFlag() {
-        assertTrue(MapUtil.parseBooleanFlag(testParameters, "trueFlag", true));
-        assertTrue(MapUtil.parseBooleanFlag(testParameters, "trueFlag", false));
-        assertTrue(MapUtil.parseBooleanFlag(testParameters, "notPresentFlag", true));
-        assertTrue(MapUtil.parseBooleanFlag(testParameters, "emptyFlag", true));
+        assertTrue(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "trueFlag", true));
+        assertTrue(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "trueFlag", false));
+        assertTrue(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "notPresentFlag", true));
+        assertTrue(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "emptyFlag", true));
 
-        assertFalse(MapUtil.parseBooleanFlag(testParameters, "falseFlag", true));
-        assertFalse(MapUtil.parseBooleanFlag(testParameters, "notPresentFlag", false));
-        assertFalse(MapUtil.parseBooleanFlag(testParameters, "emptyFlag", false));
+        assertFalse(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "falseFlag", true));
+        assertFalse(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "notPresentFlag", false));
+        assertFalse(MapUtil.parseBooleanFlag(TEST_PARAMETERS, "emptyFlag", false));
 
-        assertThrows(ContentException.class, () -> MapUtil.parseBooleanFlag(testParameters, "incorrectTypeFlag1", true));
-        assertThrows(ContentException.class, () -> MapUtil.parseBooleanFlag(testParameters, "incorrectTypeFlag2", true));
+        assertThrows(ContentException.class, () -> MapUtil.parseBooleanFlag(TEST_PARAMETERS, "incorrectTypeFlag1", true));
+        assertThrows(ContentException.class, () -> MapUtil.parseBooleanFlag(TEST_PARAMETERS, "incorrectTypeFlag2", true));
     }
 }
